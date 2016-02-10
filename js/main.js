002ece530a75c1fbad0d898d1351d320869a919c
Imac 24 <javierduarte@iMac-de-Javier-2.local>
2016-02-10 17:23:06 -0430

    primer commit

---
+++ js/main.js
@@ -0,0 +1,25 @@
+var i = 0;
+var timeout;
+$(document).ready(function(){
+    var doAnimate = function(){
+        $('.banner .slider img.active').addClass('out');
+        if($('.banner .slider img.active').index()===($('.slider').children().size() - 1))
+            $('.banner .slider img:nth(0)').addClass('next').addClass('in');
+        else
+            $('.banner .slider img.active + img').addClass('next').addClass('in');
+    };
+
+    $('.press').click(function(e){
+        $('.contmenu').toggleClass('expand');
+        e.preventDefault();
+    });
+    $('body').bind("webkitTransitionEnd oTransitionEnd otransitionend transitionend msTransitionEnd", function(){
+        $('.banner .slider img.active.out').removeClass('active').removeClass('out');
+        $('.banner .slider img.next.in').addClass('active').removeClass('next').removeClass('in');
+        clearTimeout(timeout);
+        timeout = setTimeout(doAnimate,5000);
+    });
+    clearTimeout(timeout);
+    timeout = setTimeout(doAnimate,5000);
+
+});
