50a275004de92fb12eb2e51f75718fe3720a06a1
Doctrine Dark <dctrndrk@gmail.com>
2017-03-06 23:41:25 -0400

    Subiendo al hosting

--- nob/app/config/database.php
+++ nob/app/config/database.php
@@ -55,9 +55,9 @@ return array(
         'mysql' => array(
             'driver'    => 'mysql',
             'host'      => 'localhost',
-            'database'  => '',
-            'username'  => '',
-            'password'  => '',
+            'database'  => 'jaspe_db',
+            'username'  => 'jaspe',
+            'password'  => '7PwvBdDJzFvSBwyt',
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',
