85b1b3ad8c463693a7954768587df41ce50de84d
Doctrine Dark <dctrndrk@gmail.com>
2017-03-07 11:01:16 -0400

    productos encapsulados

--- nob/app/database/seeds/ProductsTableSeeder.php
+++ nob/app/database/seeds/ProductsTableSeeder.php
@@ -11,27 +11,24 @@ class ProductsTableSeeder extends NobSeeder
 
         Session::put('translate_language','es');
 
-        $categories = Categoria::isLocaleTranslated()->active()->get();
-
-        foreach( $categories as $category )
+        for( $i = 1; $i <= 4; $i++ )
         {
-            for( $i = 1; $i <= 4; $i++ )
-            {
-                $product = Producto::createRow([
-                    'categoria'       => $category->getKey(),
-                    'nombre'          => $faker->sentence(3),
-                    'descripcion'     => $faker->text(200),
-                    'caracteristicas' => '<p><strong>'.$faker->sentence(2).'</strong><br /> '.$faker->text(100).'</p>'
-                                        .'<p><strong>'.$faker->sentence(2).'</strong><br /> '.$faker->text(100).'</p>',
-                    'tip'             => $faker->text(100),
-                    'status'          => 'active',
-                    'highlighted'     => 'yes'
-                ]);
+            $category = Categoria::isLocaleTranslated()->active()->random()->first();
 
-                if( $product->model instanceof \Nob\Admin\Model\NobBase )
-                {
-                    $this->generateFiles(1,400,400,$faker,$product->model, 'jpg', public_path("img/2x/pro{$i}.jpg"));
-                }
+            $product = Producto::createRow([
+                'categoria'       => $category->getKey(),
+                'nombre'          => $faker->sentence(3),
+                'descripcion'     => $faker->text(200),
+                'caracteristicas' => '<p><strong>'.$faker->sentence(2).'</strong><br /> '.$faker->text(100).'</p>'
+                                    .'<p><strong>'.$faker->sentence(2).'</strong><br /> '.$faker->text(100).'</p>',
+                'tip'             => $faker->text(100),
+                'status'          => 'active',
+                'highlighted'     => 'yes'
+            ]);
+
+            if( $product->model instanceof \Nob\Admin\Model\NobBase )
+            {
+                $this->generateFiles(1,400,400,$faker,$product->model, 'jpg', public_path("assets/assets/productos/pro{$i}.jpg"));
             }
         }
     }
